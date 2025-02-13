0713c16bc53eca0c5b8c7a81169bd15dfefbaf17
test <test>
2025-02-13 17:37:09 +0800

    edit cmake file

--- qnx700.cmake
+++ qnx700.cmake
@@ -8,15 +8,8 @@ set(QNX_TARGET "/opt/qnx/qnx700_17.3/target/qnx7")
 set(QNX_BASE "${QNX_TARGET}")
 
 # 编译器路径
-set(CMAKE_C_COMPILER "${QNX_HOST}/usr/bin/aarch64-unknown-nto-qnx7.1.0-gcc")
-set(CMAKE_AR "${QNX_HOST}/usr/bin/aarch64-unknown-nto-qnx7.1.0-ar")
+set(CMAKE_C_COMPILER "${QNX_HOST}/usr/bin/aarch64-unknown-nto-qnx7.0.0-gcc")
+set(CMAKE_AR "${QNX_HOST}/usr/bin/aarch64-unknown-nto-qnx7.0.0-ar")
 
 # 全局编译选项
 set(CMAKE_C_FLAGS_INIT "-O2 -fPIC -shared -Wall -D_QNX_UNIX_ -fvisibility=hidden")
-
-# 包含路径和链接路径
-include_directories(
-    "/10-common/include/leapssl"
-    "${QNX_BASE}/target/qnx7/usr/include"
-)
-link_directories("/10-common/lib/release/qnx710_4.1")
